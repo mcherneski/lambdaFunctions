012cddd115d4beedb0fb90c569c751077d1f6bde
Mike Cherneski (WSL) <mgcherneski@gmail.com>
2023-10-26 09:31:28 -0600

    Updated ValidateGeoLocation according to copilot

--- PoA_ValidateGeoLocation/index.mjs
+++ PoA_ValidateGeoLocation/index.mjs
@@ -1,10 +1,36 @@
-// TODO: Receive latitude and longitude from API Gateway and check database for valid location.
+// Todo: Upload token image to S3 and return the url for input to dynamodb
+import { GeoDataManagerConfiguration, GeoDataManager } from 'dynamodb-geo-v3'
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+
+const ddb = new DynamoDB()
+const geoConfig = new GeoDataManagerConfiguration(ddb, 'ProofOfAdventure_Locations')
+const geoTableManager = new GeoDataManager(geoConfig)
 
 export const handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+  console.log(event)
+  try {
+    const data = await geoTableManager.queryRadius({
+      RadiusInMeter: 1000,
+      CenterPoint: {
+        latitude: event.locationLat,
+        longitude: event.locationLng
+      }
+    }).promise()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        results: data
+      })
+    }
+  } catch (error) {
+    console.log(error);
+
+    return { 
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Internal Service Error'
+      })
+    }
+  }
 };
